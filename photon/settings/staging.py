7b85ae3b17cd62c0aef91b05483001c9ff573055
CristhGunners <gunners_one@hotmail.com>
2015-03-16 16:58:31 -0300

    Update Staging

---
+++ photon/settings/staging.py
@@ -0,0 +1,18 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd59r57o31gva97',
+        'USER': 'wtswxmtgwnpldy',
+        'PASSWORD:': 'y4efOxQMSqeefSzqo1KJdoj55Z',
+        'HOST': 'ec2-23-23-210-37.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR.child('media')
